a287f5b5d86f639d0044dc81991b78b9e560fec0
Anirban Shaw <anirban.shaw@bms.com>
2026-01-12 12:00:40 +0000

    update tests

--- tests/testthat/test-warprrr.R
+++ tests/testthat/test-warprrr.R
@@ -269,8 +269,6 @@ testthat::describe("warprrr utility functions", {
       Sys.sleep(0.1)
     })
     expect_type(result, "double")
-    print("=====================================")
-    print(result)
     expect_true(result >= 0.1)
   })
 
